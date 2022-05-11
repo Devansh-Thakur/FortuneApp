f05e9eca152b0c2c9b0e6ff20e6f175c2d8f01d8
DELL <devansh2000thakur@gmail.com>
2022-05-11 17:11:54 +0530

    hello

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun findFortuneFun(view: View) {
+        val name=personName.editableText.toString()
+        val intent = Intent(this,FortuneTellerActivity::class.java)
+        intent.putExtra(FortuneTellerActivity.NAME_EXTRA,name)
+        startActivity(intent)
+
+    }
+}
+
